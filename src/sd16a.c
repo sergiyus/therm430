7278554c1a03c5aa335cff29b585b6573ae5f05c
Sergiy Us <sergiy.us@gmail.com>
2025-04-22 11:50:02 +0300

    Modify Makefile and change project structure
    1. Added the ability to choose the compiler:
       - You can now use either the energia.nu compiler or MSP430-GCC-OPENSOURCE
         by setting the COMPILER variable:
         - For energia: COMPILER := energia
         - For MSP430-GCC: COMPILER := gcc
         - Alternatively, you can specify it when running the command:
           make COMPILER=gcc
    
    2. Added the ability to choose the build mode (debug or release).
    
    3. Added the option to select the MSP430 Debug Probe (eZ430 or eZ-FET).
    
    4. Changed the project structure: added src and include directories for better
       organization of source code and headers.
    
    Signed-off-by: Sergiy Us <sergiy.us@gmail.com>

---
+++ src/sd16a.c
@@ -0,0 +1,32 @@
+#include <msp430f2003.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "sd16a.h"
+
+bool voltage_sd16a_measure;
+bool temperature_sd16a_measure;
+
+bool voltage_sd16a_result;
+bool temperature_sd16a_result;
+
+bool low_battery;
+
+void start_measurement(const uint8_t sd16a_channel)
+{
+	// Ref - On
+	 SD16CTL |= SD16REFON;
+
+	// A5 - voltage devider (AVcc - AVss) / 11
+	// A6 - temperature sensor
+	SD16INCTL0 = sd16a_channel;
+
+	// SD16_A Start
+	SD16CCTL0 |= SD16SC;
+
+	if (sd16a_channel == VOLTAGE) {
+		voltage_sd16a_measure = true;
+	} else if (sd16a_channel == TEMPERATURE) {
+		temperature_sd16a_measure = true;
+	}
+}
+
